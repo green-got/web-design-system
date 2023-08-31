397113589b67a153c82d2545d7cf6f288a00fbcb
fabien-h <fabien.huet@gmail.com>
2023-08-31 23:40:23 +0200

    feat(styles): add the corners in the styles and their story

---
+++ src/Corners/Corners.stories.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import type { StoryDefault, Story } from '@ladle/react';
+import { GlobalStyles } from '../GlobalStyles/GlobalStyles';
+import styles from './Corners.module.scss';
+
+export default {
+  title: 'Foundations / Corners',
+} satisfies StoryDefault;
+
+export const Corners: Story = () => (
+  <GlobalStyles>
+    <h1>Corners</h1>
+
+    <hr />
+
+    <p>Corners are defined in the GlobalStyles scss file.</p>
+
+    <hr />
+
+    <section className={styles.container}>
+      <div>
+        <div style={{ borderRadius: 'var(--corner--xsmall)' }} />
+        <p>--corner--xsmall</p>
+      </div>
+
+      <div>
+        <div style={{ borderRadius: 'var(--corner--small)' }} />
+        <p>--corner--small</p>
+      </div>
+
+      <div>
+        <div style={{ borderRadius: 'var(--corner--medium)' }} />
+        <p>--corner--medium</p>
+      </div>
+
+      <div>
+        <div style={{ borderRadius: 'var(--corner--big)' }} />
+        <p>--corner--big</p>
+      </div>
+    </section>
+  </GlobalStyles>
+);
