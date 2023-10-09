8b999092773238470b23cae2be48db1fc4638ca2
Eamon Sisk <eamon@green-got.com>
2023-10-09 11:28:53 +0200

    chore(types): update types

--- typings/Button/Button.d.ts
+++ typings/Button/Button.d.ts
@@ -1,6 +1,9 @@
 import React from 'react';
 export type ButtonType = 'button' | 'submit';
 export interface IButtonProps {
+    attributes?: {
+        'aria-disabled': boolean;
+    };
     block?: boolean;
     className?: string;
     disabled?: boolean;
@@ -9,4 +12,4 @@ export interface IButtonProps {
     name?: string;
     type?: ButtonType;
 }
-export declare const Button: ({ block, className, children, disabled, handleClick, name, type, }: IButtonProps) => any;
+export declare const Button: ({ attributes, block, className, children, disabled, handleClick, name, type, }: IButtonProps) => any;
