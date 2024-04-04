a7ec800b504a9a9eb20e5eb953152b2765dda4b1
Eamon Sisk <eamon@green-got.com>
2024-04-04 10:43:56 +0200

    fix(switch): add className to switch

--- src/Toggles/Switch.tsx
+++ src/Toggles/Switch.tsx
@@ -1,24 +1,27 @@
 'use client';
 import React from 'react';
+import { mergeClasses } from '../utils/mergeClasses';
 import styles from './Switch.module.scss';
 
 export interface ISwitchProps {
+  className?: string;
   checked: boolean;
-  disabled: boolean;
+  disabled?: boolean;
   handleChange: () => void;
   id: string;
   label: string;
 }
 
 export function Switch({
+  className,
   checked,
-  disabled,
+  disabled = false,
   handleChange,
   id,
   label,
 }: ISwitchProps) {
   return (
-    <label className={styles.switch} htmlFor={id}>
+    <label className={mergeClasses([styles.switch, className])} htmlFor={id}>
       {label}
       <input
         aria-checked={checked}
--- typings/Toggles/Switch.d.ts
+++ typings/Toggles/Switch.d.ts
@@ -1,8 +1,9 @@
 export interface ISwitchProps {
+    className?: string;
     checked: boolean;
-    disabled: boolean;
+    disabled?: boolean;
     handleChange: () => void;
     id: string;
     label: string;
 }
-export declare function Switch({ checked, disabled, handleChange, id, label, }: ISwitchProps): any;
+export declare function Switch({ className, checked, disabled, handleChange, id, label, }: ISwitchProps): any;
