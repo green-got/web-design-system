e1d6b774555ac47b1ecb179968f40ff9a7628e8e
Eamon Sisk <eamon@green-got.com>
2024-08-20 15:17:05 +0200

    fix(dialog): fix scroll jump when showing dialog

--- src/Corners/Corners.stories.tsx
+++ src/Corners/Corners.stories.tsx
@@ -22,22 +22,22 @@ export const Corners: Story = () => (
     <section className={styles.container}>
       <div>
         <div style={{ borderRadius: 'var(--corner-xsmall)' }} />
-        <p>--corner--xsmall</p>
+        <p>--corner-xsmall</p>
       </div>
 
       <div>
         <div style={{ borderRadius: 'var(--corner-small)' }} />
-        <p>--corner--small</p>
+        <p>--corner-small</p>
       </div>
 
       <div>
         <div style={{ borderRadius: 'var(--corner-medium)' }} />
-        <p>--corner--medium</p>
+        <p>--corner-medium</p>
       </div>
 
       <div>
         <div style={{ borderRadius: 'var(--corner-big)' }} />
-        <p>--corner--big</p>
+        <p>--corner-big</p>
       </div>
     </section>
   </>
