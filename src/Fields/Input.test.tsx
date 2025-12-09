b35f0c0f73696b198410de80d205de2ed16b36ee
Eamon Sisk <eamon@green-got.com>
2025-12-09 10:54:18 +0100

    feat(v2): wip

--- src/Fields/Input.test.tsx
+++ src/Fields/Input.test.tsx
@@ -3,7 +3,9 @@ import { Input } from './Input';
 
 describe('Input', () => {
   it('renders an input', () => {
-    render(<Input handleChange={() => {}} id="a" label="Label" name="a" value="" />);
+    render(
+      <Input handleChange={() => {}} id="a" label="Label" name="a" value="" />,
+    );
     screen.debug();
   });
 });
--- src/Pagination/Pagination.tsx
+++ src/Pagination/Pagination.tsx
@@ -1,4 +1,3 @@
-
 export const Pagination = () => {
   return <div>hey</div>;
 };
