045922af5c358a00a273026d1018c635875dce28
Eamon Sisk <eamon@green-got.com>
2025-12-10 15:35:24 +0100

    feat(v2): update typings for clearable input

--- typings/Fields/ClearableInput.d.ts
+++ typings/Fields/ClearableInput.d.ts
@@ -1,2 +1,2 @@
 import type { IInputProps } from './Input';
-export declare function ClearableInput({ attributes, disabled, errorMessage, id, label, name, required, type, value, ...props }: IInputProps): import("react/jsx-runtime").JSX.Element;
+export declare function ClearableInput({ disabled, errorMessage, id, label, name, required, type, value, ...props }: IInputProps): import("react/jsx-runtime").JSX.Element;
